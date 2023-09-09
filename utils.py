ac01d1e04d137d71524a479ba4f96125345c93df
youyegit <youyexiaoyao@gmail.com>
2023-09-09 20:10:47 +0800

    add model_path to config.yaml

--- utils.py
+++ utils.py
@@ -12,13 +12,13 @@ config_path = Path(here, "config.yaml")
 if os.path.exists(config_path):
     config = yaml.load(open(config_path, "r"), Loader=yaml.FullLoader)
 
-    #annotator_ckpts_path = os.path.join(os.path.dirname(__file__), "ckpts")
     annotator_ckpts_path = str(Path(here, config["annotator_ckpts_path"]))
+    HF_MODEL_NAME = str(Path(here, config["HF_MODEL_NAME"]))
+    DWPOSE_MODEL_NAME = str(Path(here, config["DWPOSE_MODEL_NAME"]))
 else:
     annotator_ckpts_path = str(Path(here, "./ckpts"))
-
-HF_MODEL_NAME = "lllyasviel/Annotators"
-DWPOSE_MODEL_NAME = "yzd-v/DWPose"
+    HF_MODEL_NAME = "lllyasviel/Annotators"
+    DWPOSE_MODEL_NAME = "yzd-v/DWPose"
 
 def common_annotator_call(model, tensor_image, **kwargs):
     out_list = []
