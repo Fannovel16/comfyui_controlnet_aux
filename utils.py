a582f4545ae2dcef3a4b0a65de2718aef3ccee17
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-09-10 21:23:31 +0700

    Pad 64 as ControlNet Apply need multiple-of-64 resolution

--- utils.py
+++ utils.py
@@ -20,12 +20,18 @@ else:
 HF_MODEL_NAME = "lllyasviel/Annotators"
 DWPOSE_MODEL_NAME = "yzd-v/DWPose"
 
+def pad64(x):
+    return int(np.ceil(float(x) / 64.0) * 64 - x)
+
 def common_annotator_call(model, tensor_image, **kwargs):
     out_list = []
     for image in tensor_image:
-        H, W, C = image.shape
         np_image = np.asarray(image * 255., dtype=np.uint8) 
         np_result = model(np_image, output_type="np", **kwargs)
-        np_result = cv2.resize(np_result, (W, H), interpolation=cv2.INTER_AREA)
+
+        #Pad 64 as ControlNet Apply need multiple-of-64 resolution
+        H_pad, W_pad = pad64(np_result.shape[0]), pad64(np_result.shape[1])
+        np_result = np.pad(np_result, [[0, H_pad], [0, W_pad], [0, 0]], mode='edge')
+
         out_list.append(torch.from_numpy(np_result.astype(np.float32) / 255.0))
     return torch.stack(out_list, dim=0)
