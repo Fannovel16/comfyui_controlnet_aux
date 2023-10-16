6fd49ffeefba338cdda143cee7405c961034000f
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-10-16 13:48:15 +0700

    Fix https://github.com/Fannovel16/comfyui_controlnet_aux/issues/76

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ print(f"Registered sys.path: {sys.path}")
 #Enable CPU fallback for ops not being supported by MPS like upsample_bicubic2d.out
 #https://github.com/pytorch/pytorch/issues/77764
 #https://github.com/Fannovel16/comfyui_controlnet_aux/issues/2#issuecomment-1763579485
-os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = 1 
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = '1' 
 
 
 def load_nodes():
