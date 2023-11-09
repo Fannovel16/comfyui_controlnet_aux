98bf5b70a98577dcbb1227cdf4fbbf022149a1e1
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-11-09 20:40:47 +0700

    Trying to fix utils conflicts

--- __init__.py
+++ __init__.py
@@ -11,8 +11,8 @@ from .hint_image_enchance import NODE_DISPLAY_NAME_MAPPINGS as HIE_NODE_DISPLAY_
 
 #Ref: https://github.com/comfyanonymous/ComfyUI/blob/76d53c4622fc06372975ed2a43ad345935b8a551/nodes.py#L17
 sys.path.insert(0, str(Path(here, "src").resolve()))
-for pkg_name in os.listdir(str(Path(here, "src"))):
-    sys.path.insert(0, str(Path(here, "src", pkg_name).resolve()))
+for pkg_name in ["controlnet_aux", "custom_mmpkg"]:
+    sys.path.append(str(Path(here, "src", pkg_name).resolve()))
 
 #Enable CPU fallback for ops not being supported by MPS like upsample_bicubic2d.out
 #https://github.com/pytorch/pytorch/issues/77764
