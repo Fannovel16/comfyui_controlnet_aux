4f9ac2f14073159d0df5b6b2bd2faab455a2f723
Layer-norm <magicianaio@gmail.com>
2023-12-08 00:22:53 +0800

    try to fix densepose node

--- src/controlnet_aux/densepose/__init__.py
+++ src/controlnet_aux/densepose/__init__.py
@@ -1,16 +1,16 @@
 import functools
 import os
 import warnings
-
+import torchvision #lack of nmp
 import cv2
 import numpy as np
 import torch
 import torch.nn as nn
 from einops import rearrange
-from huggingface_hub import hf_hub_download
 from PIL import Image
 
 from controlnet_aux.util import HWC3, resize_image_with_pad, common_input_validate, annotator_ckpts_path, custom_hf_download
+from .densepose import civitai_vis, densepose_chart_predictor_output_to_result_with_confidences
 
 class DenseposeDetector:
     def __init__(self, model):
@@ -32,12 +32,30 @@ class DenseposeDetector:
         input_image, output_type = common_input_validate(input_image, output_type, **kwargs)
         input_image, remove_pad = resize_image_with_pad(input_image, detect_resolution, upscale_method)
         H, W  = input_image.shape[:2]
+
+        hint_image_canvas = np.zeros([H, W], dtype=np.uint8)
+        hint_image_canvas = np.tile(hint_image_canvas[:, :, np.newaxis], [1, 1, 3])
+
         input_image = rearrange(torch.from_numpy(input_image).to(self.device), 'h w c -> c h w')
-        detected_map = self.dense_pose_estimation(input_image)[-1 if cmap=="viridis" else -2]
-        if detected_map.all() == -1:
-            detected_map = np.zeros([H, W, 3], dtype=np.uint8)
-        else:
-            detected_map = detected_map.cpu().detach().numpy()
+
+        pred_boxes, corase_segm, fine_segm, u, v = self.dense_pose_estimation(input_image)
+
+        extractor = densepose_chart_predictor_output_to_result_with_confidences
+        densepose_results = [extractor(pred_boxes[i:i+1], corase_segm[i:i+1], fine_segm[i:i+1], u[i:i+1], v[i:i+1]) for i in range(len(pred_boxes))]
+
+        hint_image = civitai_vis.visualize(hint_image_canvas, densepose_results)
+        hint_image = cv2.cvtColor(hint_image, cv2.COLOR_BGR2RGB)
+        hint_image = torch.from_numpy(hint_image)
+
+        if cmap=="viridis":
+            hint_image[:, :, 0][hint_image[:, :, 0] == 0] = 68
+            hint_image[:, :, 1][hint_image[:, :, 1] == 0] = 1
+            hint_image[:, :, 2][hint_image[:, :, 2] == 0] = 84
+
+        detected_map = hint_image
+
+        detected_map = detected_map.cpu().detach().numpy()
+
         if output_type == "pil":
             detected_map = Image.fromarray(detected_map)
         detected_map = remove_pad(HWC3(detected_map))
