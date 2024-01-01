6e271355124e160b5df18ad5978a2839612a7dd6
tai17082006.tk@gmail.com <tai17082006.tk@gmail.com>
2024-01-01 17:36:15 +0700

    Fix https://github.com/Fannovel16/comfyui_controlnet_aux/issues/173

--- node_wrappers/mesh_graphormer.py
+++ node_wrappers/mesh_graphormer.py
@@ -37,6 +37,8 @@ def install_deps():
     except ImportError:
         run_script([sys.executable, '-s', '-m', 'pip', 'install', 'trimesh', 'pyembree', 'embreex', 'yacs'])
 
+    run_script([sys.executable, '-s', '-m', 'pip', 'install', 'setuptools>=67.0.0'])
+
 class Mesh_Graphormer_Depth_Map_Preprocessor:
     @classmethod
     def INPUT_TYPES(s):
