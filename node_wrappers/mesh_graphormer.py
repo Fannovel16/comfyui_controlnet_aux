66349017c51416d3636567963d849d72823dbfaa
tai17082006.tk@gmail.com <tai17082006.tk@gmail.com>
2024-01-04 20:40:09 +0700

    End lagbrain

--- node_wrappers/mesh_graphormer.py
+++ node_wrappers/mesh_graphormer.py
@@ -56,11 +56,13 @@ class Mesh_Graphormer_Depth_Map_Preprocessor:
     @classmethod
     def INPUT_TYPES(s):
         types = create_node_input_types(mask_bbox_padding=("INT", {"default": 30, "min": 0, "max": 100}))
-        types.update(dict(optional={
+        types["optional"].update(
+            {
                 "mask_type": (["based_on_depth", "original"], {"default": "based_on_depth"}),
                 "mask_expand": ("INT", {"default": 5, "min": -MAX_RESOLUTION, "max": MAX_RESOLUTION, "step": 1}),
                 "rand_seed": ("INT", {"default": 88, "min": 0, "max": 0xffffffffffffffff})
-            }))
+            }
+        )
         return types
 
     RETURN_TYPES = ("IMAGE", "MASK")
