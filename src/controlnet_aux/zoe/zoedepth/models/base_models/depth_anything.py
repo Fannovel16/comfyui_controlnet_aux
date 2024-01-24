6303159c990f1a36cefe56927ae992565bfcdaa3
Hacker 17082006 <tai17082006.tk@gmail.com>
2024-01-24 13:36:41 +0700

    'i'

--- src/controlnet_aux/zoe/zoedepth/models/base_models/depth_anything.py
+++ src/controlnet_aux/zoe/zoedepth/models/base_models/depth_anything.py
@@ -172,8 +172,7 @@ class Resize(object):
 
     def __call__(self, x):
         width, height = self.get_size(*x.shape[-2:][::-1])
-        width, height = int(width), int(height)
-        return nn.functional.interpolate(x, (height, width), mode='bilinear', align_corners=True)
+        return nn.functional.interpolate(x, (int(width), int(height)), mode='bilinear', align_corners=True)
 
 class PrepForMidas(object):
     def __init__(self, resize_mode="minimal", keep_aspect_ratio=True, img_size=384, do_resize=True):
