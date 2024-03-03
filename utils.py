9f8b386848637ad48bbcbd42824e315f64a0341d
Hacker 17082006 <tai17082006.tk@gmail.com>
2024-03-03 11:24:53 +0700

    Add progress bar and optimize adding for-loop in common_annotator_call

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from enum import Enum
 from .log import log
 import subprocess
 import threading
-import sys
+import comfy
 
 here = Path(__file__).parent.resolve()
 
@@ -61,12 +61,18 @@ def common_annotator_call(model, tensor_image, input_batch=False, **kwargs):
         np_results = model(np_images, output_type="np", detect_resolution=detect_resolution, **kwargs)
         return torch.from_numpy(np_results.astype(np.float32) / 255.0)
 
-    out_list = []
-    for image in tensor_image:
+    batch_size = tensor_image.shape[0]
+    pbar = comfy.utils.ProgressBar(batch_size)
+    out_tensor = None
+    for i, image in enumerate(tensor_image):
         np_image = np.asarray(image.cpu() * 255., dtype=np.uint8)
         np_result = model(np_image, output_type="np", detect_resolution=detect_resolution, **kwargs)
-        out_list.append(torch.from_numpy(np_result.astype(np.float32) / 255.0))
-    return torch.stack(out_list, dim=0)
+        out = torch.from_numpy(np_result.astype(np.float32) / 255.0)
+        if out_tensor is None:
+            out_tensor = torch.zeros(batch_size, *out.shape, dtype=torch.float32)
+        out_tensor[i] = out
+        pbar.update(1)
+    return out_tensor
 
 def create_node_input_types(**extra_kwargs):
     return {
