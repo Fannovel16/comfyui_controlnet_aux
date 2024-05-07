cb4ce92cdba21628ab94ce2c8fe0541ec47564a1
Hacker 17082006 <tai17082006.tk@gmail.com>
2024-05-07 12:00:26 +0700

    Remove Zluda or whatever bug for not supporting Sparse tensor

--- src/wrapper_for_mps/__init__.py
+++ src/wrapper_for_mps/__init__.py
@@ -4,6 +4,4 @@ from comfy.model_management import get_torch_device
 device = get_torch_device()
 #https://github.com/microsoft/DirectML/issues/414#issuecomment-1541319479
 def sparse_to_dense(sparse_tensor):
-    if device.type in ['dml', 'mps', 'xpu', 'privateuseone']:
-        return sparse_tensor.to_dense()
-    return sparse_tensor
+    return sparse_tensor.to_dense()
