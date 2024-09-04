733fcb4f2167266dad2c727fc162d276ee8eaac0
H.D.Tài <tai17082006.tk@gmail.com>
2024-09-05 01:01:32 +0700

    Not forget xinsir this time

--- node_wrappers/inpaint.py
+++ node_wrappers/inpaint.py
@@ -6,18 +6,18 @@ class InpaintPreprocessor:
     def INPUT_TYPES(s):
         return dict(
             required=dict(image=INPUT.IMAGE(), mask=INPUT.MASK()),
-            optional=dict(black_pixel_for_xinsr_cn=INPUT.BOOLEAN(False))
+            optional=dict(black_pixel_for_xinsir_cn=INPUT.BOOLEAN(False))
         )
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "preprocess"
 
     CATEGORY = "ControlNet Preprocessors/others"
 
-    def preprocess(self, image, mask, black_pixel_for_xinsr_cn=False):
+    def preprocess(self, image, mask, black_pixel_for_xinsir_cn=False):
         mask = torch.nn.functional.interpolate(mask.reshape((-1, 1, mask.shape[-2], mask.shape[-1])), size=(image.shape[1], image.shape[2]), mode="bilinear")
         mask = mask.movedim(1,-1).expand((-1,-1,-1,3))
         image = image.clone()
-        if black_pixel_for_xinsr_cn:
+        if black_pixel_for_xinsir_cn:
             masked_pixel = 0.0
         else:
             masked_pixel = -1.0
