2d19d298a224a4a82fd60c3fe1cd51e2d77cb9cc
SB Yoon <44089734+yansigit@users.noreply.github.com>
2025-03-10 22:45:29 -0600

    Update modeling_bert.py

--- src/custom_mesh_graphormer/modeling/bert/modeling_bert.py
+++ src/custom_mesh_graphormer/modeling/bert/modeling_bert.py
@@ -1,1 +1,5 @@
-from transformers.models.bert.modeling_bert import *
+from transformers.models.bert import modeling_bert
+
+for symbol in dir(modeling_bert):
+    if not symbol.startswith("_"):
+        globals()[symbol] = getattr(modeling_bert, symbol)
