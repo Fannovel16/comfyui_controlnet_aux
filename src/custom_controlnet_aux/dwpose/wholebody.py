d45da9b30300ea86ed3f232edc2904810b9baba0
toyxyz <toyxyz@naver.com>
2025-05-30 01:45:24 +0900

    Update wholebody.py

--- src/custom_controlnet_aux/dwpose/wholebody.py
+++ src/custom_controlnet_aux/dwpose/wholebody.py
@@ -78,18 +78,25 @@ class Wholebody:
 
     def __call__(self, oriImg) -> Optional[np.ndarray]:
         #Sacrifice accurate time measurement for compatibility 
-        det_start = default_timer()
-        if is_model_torchscript(self.det):
-            det_result = inference_jit_yolox(self.det, oriImg, detect_classes=[0])
+        
+        det_result = None
+        
+        if self.det is None:
+            print("DWPose: No detector specified, using full image for pose estimation.") # pragma: no cover
+            det_result = []
         else:
-            if "yolox" in self.det_filename:
-                det_result = inference_onnx_yolox(self.det, oriImg, detect_classes=[0], dtype=np.float32)
+            det_start = default_timer()
+            if is_model_torchscript(self.det):
+                det_result = inference_jit_yolox(self.det, oriImg, detect_classes=[0])
             else:
-                #FP16 and INT8 YOLO NAS accept uint8 input
-                det_result = inference_onnx_yolo_nas(self.det, oriImg, detect_classes=[0], dtype=np.uint8)
-        print(f"DWPose: Bbox {((default_timer() - det_start) * 1000):.2f}ms")
-        if (det_result is None) or (det_result.shape[0] == 0):
-            return None
+                if "yolox" in self.det_filename:
+                    det_result = inference_onnx_yolox(self.det, oriImg, detect_classes=[0], dtype=np.float32)
+                else:
+                    #FP16 and INT8 YOLO NAS accept uint8 input
+                    det_result = inference_onnx_yolo_nas(self.det, oriImg, detect_classes=[0], dtype=np.uint8)
+            print(f"DWPose: Bbox {((default_timer() - det_start) * 1000):.2f}ms")
+            if (det_result is None) or (det_result.shape[0] == 0):
+                return None
 
         pose_start = default_timer()
         if is_model_torchscript(self.pose):
@@ -97,7 +104,14 @@ class Wholebody:
         else:
             _, pose_onnx_dtype = guess_onnx_input_shape_dtype(self.pose_filename)
             keypoints, scores = inference_onnx_pose(self.pose, det_result, oriImg, self.pose_input_size, dtype=pose_onnx_dtype)
-        print(f"DWPose: Pose {((default_timer() - pose_start) * 1000):.2f}ms on {det_result.shape[0]} people\n")
+            
+        num_subjects_log = 'full image'
+        if hasattr(det_result, 'shape') and det_result.shape[0] > 0:
+            num_subjects_log = f"{det_result.shape[0]} people"
+        elif isinstance(det_result, list) and len(det_result) > 0 and isinstance(det_result[0], (list, np.ndarray)):
+             num_subjects_log = f"{len(det_result)} people"
+        
+        print(f"DWPose: Pose {((default_timer() - pose_start) * 1000):.2f}ms on {num_subjects_log}\n")
 
         keypoints_info = np.concatenate(
             (keypoints, scores[..., None]), axis=-1)
