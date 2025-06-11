2e4e7f68df6378a3e13685d0144dfc24497ac221
Test User <test@example.com>
2025-06-11 13:18:52 +0300

    chore: delete unused code, fix permissions

--- src/custom_timm/utils/random.py
+++
@@ -1,9 +0,0 @@
-import random
-import numpy as np
-import torch
-
-
-def random_seed(seed=42, rank=0):
-    torch.manual_seed(seed + rank)
-    np.random.seed(seed + rank)
-    random.seed(seed + rank)
