8859eea0814baa6c8d02e8572bd54101432b4f03
yarik1988 <yaroslav.linder@gmail.com>
2025-09-02 13:37:47 +0300

    lora->loras

--- provisioning_scripts_vast/comfyui_kontext.sh
+++ provisioning_scripts_vast/comfyui_kontext.sh
@@ -80,7 +80,7 @@ function provisioning_start() {
         "${COMFYUI_DIR}/models/unet" \
         "${UNET_MODELS[@]}"
     provisioning_get_files \
-        "${COMFYUI_DIR}/models/lora" \
+        "${COMFYUI_DIR}/models/loras" \
         "${LORA_MODELS[@]}"
     provisioning_get_files \
         "${COMFYUI_DIR}/models/controlnet" \
