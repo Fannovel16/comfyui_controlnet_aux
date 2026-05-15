32551299d79a59197419b938f252eb614b0f1871
Dw_03 <madduck634858934@protonmail.com>
2026-05-15 13:17:31 +0200

    Update dwpose.py

--- node_wrappers/dwpose.py
+++ node_wrappers/dwpose.py
@@ -8,7 +8,7 @@ import json
 
 DWPOSE_MODEL_NAME = "yzd-v/DWPose"
 #Trigger startup caching for onnxruntime
-GPU_PROVIDERS = ["CUDAExecutionProvider", "DirectMLExecutionProvider", "OpenVINOExecutionProvider", "ROCMExecutionProvider", "CoreMLExecutionProvider"]
+GPU_PROVIDERS = ["CUDAExecutionProvider", "DirectMLExecutionProvider", "OpenVINOExecutionProvider", "ROCMExecutionProvider", "MIGraphXExecutionProvider", "CPUExecutionProvider"]
 def check_ort_gpu():
     try:
         import onnxruntime as ort
